5b6e1234c807c181cc48d9192bea2639b0ca1902
Jim Blackler <jimblackler@google.com>
2014-10-21 08:54:17 +0100

    "Numbers" demo used in docs.

---
+++ sample_data/numbers.js
@@ -0,0 +1,23 @@
+demo({
+  "tree": "10\n 20\n  80\n   90\n 30\n 40\n  50\n  55\n   61\n  60\n   62\n   64\n    65\n  70 \n 45",
+  "options": {
+    "flipXY": 0,
+    "width": 320,
+    "height": 450,
+    "labelLineSpacing": 15,
+    "cornerRounding": 0,
+    "labelPadding": 2,
+    "arrowHeadSize": 5,
+    "arrowsUp": 0,
+    "siblingGap": 0.1,
+    "idealSiblingGap": 0.1,
+    "minimumCousinGap": 0.2,
+    "idealCousinGap": 1.2,
+    "levelsGap": 1.2,
+    "minimumDepth": 0,
+    "minimumBreadth": 0,
+    "drawRoot": false,
+    "special": 0
+  },
+  "styles": "text {\n  text-anchor: middle;\n  font-size: small;\n  font-weight: bold;\n  fill: gray;\n}\n\nrect {\n  fill: white;\n  stroke: black;\n  stroke-width: 1;\n}\n\nline {\n  stroke: black;\n  opacity: 0.5;\n  stroke-width: 1;\n  stroke-dasharray: 3, 2;\n}\n "
+} );
